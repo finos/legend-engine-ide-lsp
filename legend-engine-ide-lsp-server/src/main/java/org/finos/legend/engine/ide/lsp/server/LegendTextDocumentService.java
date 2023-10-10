c53e31146fd1adf8869497516e47d680d122794a
kevin-m-knight-gs <kevin.knight@gs.com>
2023-10-10 14:51:50 -0400

    Expand document state in LegendTextDocumentService

--- legend-engine-ide-lsp-server/src/main/java/org/finos/legend/engine/ide/lsp/server/LegendTextDocumentService.java
+++ legend-engine-ide-lsp-server/src/main/java/org/finos/legend/engine/ide/lsp/server/LegendTextDocumentService.java
@@ -18,14 +18,17 @@ import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
 import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.TextDocumentContentChangeEvent;
 import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.eclipse.lsp4j.TextDocumentItem;
 import org.eclipse.lsp4j.VersionedTextDocumentIdentifier;
 import org.eclipse.lsp4j.services.TextDocumentService;
+import org.finos.legend.engine.ide.lsp.tools.TextTools;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -77,14 +80,13 @@ class LegendTextDocumentService implements TextDocumentService
                 {
                     LOGGER.debug("no changes to {}", uri);
                 }
-                else if (changes.size() == 1)
-                {
-                    state.text = changes.get(0).getText();
-                }
                 else
                 {
-                    LOGGER.warn("cannot process changes to {}: expected 1 change, got {}", uri, changes.size());
-                    state.text = null;
+                    if (changes.size() > 1)
+                    {
+                        LOGGER.warn("Expected at most one change, got: {}", changes.size());
+                    }
+                    state.setText(changes.get(0).getText());
                 }
             }
         }
@@ -124,7 +126,7 @@ class LegendTextDocumentService implements TextDocumentService
                 String text = params.getText();
                 if (text != null)
                 {
-                    state.text = text;
+                    state.setText(text);
                 }
             }
         }
@@ -139,11 +141,82 @@ class LegendTextDocumentService implements TextDocumentService
     {
         private int version;
         private String text;
+        private int[] lineIndex;
 
         private DocumentState(int version, String text)
         {
             this.version = version;
-            this.text = text;
+            setText(text);
+        }
+
+        int getLineStart(int line)
+        {
+            checkLine(line);
+            return getLineStart_internal(line);
+        }
+
+        int getLineEnd(int line)
+        {
+            checkLine(line);
+            return getLineEnd_internal(line);
+        }
+
+        int getIndex(Position position)
+        {
+            return getIndex(position.getLine(), position.getCharacter());
+        }
+
+        int getIndex(int line, int character)
+        {
+            checkLine(line);
+            int lineStart = getLineStart_internal(line);
+            int lineEnd = getLineEnd_internal(line);
+            int lineLen = lineEnd - lineStart;
+            if (character >= lineLen)
+            {
+                throw new IndexOutOfBoundsException("Invalid character offset in line " + line + ": " + character + " (line length: " + lineLen + ")");
+            }
+            return lineStart + character;
+        }
+
+        int getLine(int index)
+        {
+            if ((index < 0) || (index >= this.text.length()))
+            {
+                throw new StringIndexOutOfBoundsException("index " + index + ", length " + this.text.length());
+            }
+            return getLine_internal(index);
+        }
+
+        void setText(String newText)
+        {
+            this.text = newText;
+            this.lineIndex = TextTools.indexLines(newText);
+        }
+
+        private int getLineStart_internal(int line)
+        {
+            return this.lineIndex[line];
+        }
+
+        private int getLineEnd_internal(int line)
+        {
+            int nextLine = line + 1;
+            return (nextLine == this.lineIndex.length) ? this.text.length() : this.lineIndex[nextLine];
+        }
+
+        private int getLine_internal(int index)
+        {
+            int i = Arrays.binarySearch(this.lineIndex, index);
+            return (i >= 0) ? i : -(i + 2);
+        }
+
+        private void checkLine(int line)
+        {
+            if ((line < 0) || (line >= this.lineIndex.length))
+            {
+                throw new IndexOutOfBoundsException("Invalid line: " + line);
+            }
         }
     }
 }
