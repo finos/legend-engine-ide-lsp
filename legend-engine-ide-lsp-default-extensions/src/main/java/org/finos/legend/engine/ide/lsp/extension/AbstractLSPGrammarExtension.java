75df6b067050600197d126632375d00df4122163
kevin-m-knight-gs <kevin.knight@gs.com>
2023-10-23 18:21:15 -0400

    Add some default implementations of getDeclarations

---
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/AbstractLSPGrammarExtension.java
@@ -0,0 +1,115 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.ide.lsp.extension;
+
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
+import org.finos.legend.engine.ide.lsp.extension.declaration.LegendDeclaration;
+import org.finos.legend.engine.ide.lsp.extension.text.GrammarSection;
+import org.finos.legend.engine.ide.lsp.extension.text.TextInterval;
+import org.finos.legend.engine.language.pure.grammar.from.ParseTreeWalkerSourceInformation;
+import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParserContext;
+import org.finos.legend.engine.language.pure.grammar.from.SectionSourceCode;
+import org.finos.legend.engine.language.pure.grammar.from.extension.PureGrammarParserExtensions;
+import org.finos.legend.engine.protocol.pure.v1.model.SourceInformation;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.PackageableElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.function.Consumer;
+
+abstract class AbstractLSPGrammarExtension implements LegendLSPGrammarExtension
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractLSPGrammarExtension.class);
+
+    @Override
+    public Iterable<? extends LegendDeclaration> getDeclarations(GrammarSection section)
+    {
+        if (!getName().equals(section.getGrammar()))
+        {
+            LOGGER.warn("Cannot handle grammar {} in extension {}", section.getGrammar(), getName());
+            return Collections.emptyList();
+        }
+
+
+        PureGrammarParserContext parserContext = new PureGrammarParserContext(PureGrammarParserExtensions.fromAvailableExtensions());
+        MutableList<LegendDeclaration> declarations = Lists.mutable.empty();
+        parse(toSectionSourceCode(section), element ->
+        {
+            LegendDeclaration declaration = getDeclaration(element);
+            if (declaration != null)
+            {
+                declarations.add(declaration);
+            }
+        }, parserContext);
+        return declarations;
+    }
+
+    protected abstract void parse(SectionSourceCode section, Consumer<PackageableElement> elementConsumer, PureGrammarParserContext pureGrammarParserContext);
+
+    protected LegendDeclaration getDeclaration(PackageableElement element)
+    {
+        String path = element.getPath();
+        TextInterval location = toLocation(element.sourceInformation);
+        if (location == null)
+        {
+            LOGGER.warn("No source information for {}", path);
+            return null;
+        }
+
+        String classifier = getClassifier(element);
+        if (classifier == null)
+        {
+            LOGGER.warn("No classifier for {}", path);
+            return null;
+        }
+
+        LegendDeclaration.Builder builder = LegendDeclaration.builder()
+                .withIdentifier(path)
+                .withClassifier(classifier)
+                .withLocation(location);
+        forEachChild(element, child ->
+        {
+            if (child != null)
+            {
+                builder.withChild(child);
+            }
+        });
+        return builder.build();
+    }
+
+    protected abstract String getClassifier(PackageableElement element);
+
+    protected TextInterval toLocation(SourceInformation sourceInfo)
+    {
+        return (sourceInfo == null) ?
+                null :
+                TextInterval.newInterval(sourceInfo.startLine, sourceInfo.startColumn, sourceInfo.endLine, sourceInfo.endColumn);
+    }
+
+    protected void forEachChild(PackageableElement element, Consumer<LegendDeclaration> consumer)
+    {
+        // Do nothing by default
+    }
+
+    private SectionSourceCode toSectionSourceCode(GrammarSection section)
+    {
+        String souceId = "section_" + section.getGrammar() + ".pure";
+        SourceInformation sectionSourceInfo = new SourceInformation(souceId, section.getStartLine(), 0, section.getEndLine(), section.getLineLength(section.getEndLine()));
+        ParseTreeWalkerSourceInformation walkerSourceInfo = new ParseTreeWalkerSourceInformation.Builder(souceId, section.getStartLine(), 0).build();
+        return new SectionSourceCode(section.getText(), section.getGrammar(), sectionSourceInfo, walkerSourceInfo);
+    }
+}
