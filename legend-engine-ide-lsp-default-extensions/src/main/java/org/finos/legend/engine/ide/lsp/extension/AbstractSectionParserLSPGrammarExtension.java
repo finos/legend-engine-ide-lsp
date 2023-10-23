79f6cf5ef1aae2e808d75bbf79f17f11eeeabddf
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2023-10-23 18:25:08 -0400

    Add some default implementations of getDeclarations (#57)

---
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/AbstractSectionParserLSPGrammarExtension.java
@@ -0,0 +1,44 @@
+// Copyright 2023 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.ide.lsp.extension;
+
+import org.finos.legend.engine.language.pure.grammar.from.PureGrammarParserContext;
+import org.finos.legend.engine.language.pure.grammar.from.SectionSourceCode;
+import org.finos.legend.engine.language.pure.grammar.from.extension.SectionParser;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.PackageableElement;
+
+import java.util.function.Consumer;
+
+abstract class AbstractSectionParserLSPGrammarExtension<T extends SectionParser> extends AbstractLSPGrammarExtension
+{
+    protected final T parser;
+
+    protected AbstractSectionParserLSPGrammarExtension(T parser)
+    {
+        this.parser = parser;
+    }
+
+    @Override
+    public String getName()
+    {
+        return this.parser.getSectionTypeName();
+    }
+
+    @Override
+    protected void parse(SectionSourceCode section, Consumer<PackageableElement> elementConsumer, PureGrammarParserContext parserContext)
+    {
+        this.parser.parse(section, elementConsumer, parserContext);
+    }
+}
