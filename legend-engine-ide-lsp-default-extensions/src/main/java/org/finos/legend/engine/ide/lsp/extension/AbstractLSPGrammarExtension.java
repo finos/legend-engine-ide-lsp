85511f79196b2b7f18599a7d4fa6c483250c02a5
Kevin Knight <57677197+kevin-m-knight-gs@users.noreply.github.com>
2023-11-07 17:59:52 -0500

    Store the result of parsing in section state (#75)

--- legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/AbstractLSPGrammarExtension.java
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/AbstractLSPGrammarExtension.java
@@ -15,6 +15,7 @@
 package org.finos.legend.engine.ide.lsp.extension;
 
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.ImmutableList;
 import org.eclipse.collections.api.list.MutableList;
 import org.finos.legend.engine.ide.lsp.extension.declaration.LegendDeclaration;
 import org.finos.legend.engine.ide.lsp.extension.diagnostic.LegendDiagnostic;
@@ -37,11 +38,13 @@ abstract class AbstractLSPGrammarExtension implements LegendLSPGrammarExtension
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractLSPGrammarExtension.class);
 
+    private static final String PARSE_RESULT = "parse";
+
     @Override
     public Iterable<? extends LegendDeclaration> getDeclarations(SectionState sectionState)
     {
         MutableList<LegendDeclaration> declarations = Lists.mutable.empty();
-        parse(toSectionSourceCode(sectionState), element ->
+        getParseResult(sectionState).forEachElement(element ->
         {
             LegendDeclaration declaration = getDeclaration(element);
             if (declaration != null)
@@ -62,15 +65,10 @@ abstract class AbstractLSPGrammarExtension implements LegendLSPGrammarExtension
 
     private void collectParserDiagnostics(SectionState sectionState, Consumer<? super LegendDiagnostic> consumer)
     {
-        try
+        ParseResult parseResult = getParseResult(sectionState);
+        if (parseResult.hasEngineException())
         {
-            parse(toSectionSourceCode(sectionState), e ->
-            {
-                // do nothing
-            });
-        }
-        catch (EngineException e)
-        {
+            EngineException e = parseResult.getEngineException();
             SourceInformation sourceInfo = e.getSourceInformation();
             if (isValidSourceInfo(sourceInfo))
             {
@@ -81,15 +79,39 @@ abstract class AbstractLSPGrammarExtension implements LegendLSPGrammarExtension
                 LOGGER.warn("Invalid source information in parser exception in section {} of {}: cannot create diagnostic", sectionState.getSectionNumber(), sectionState.getDocumentState().getDocumentId(), e);
             }
         }
+    }
+
+    protected ParseResult getParseResult(SectionState sectionState)
+    {
+        return sectionState.getProperty(PARSE_RESULT, () -> tryParse(sectionState));
+    }
+
+    protected ParseResult tryParse(SectionState sectionState)
+    {
+        MutableList<PackageableElement> elements = Lists.mutable.empty();
+        try
+        {
+            parse(sectionState, elements::add);
+            return new ParseResult(elements.toImmutable());
+        }
         catch (Exception e)
         {
-            LOGGER.error("Unexpected exception when parsing section {} of {}: cannot create diagnostic", sectionState.getSectionNumber(), sectionState.getDocumentState().getDocumentId(), e);
+            if (!(e instanceof EngineException))
+            {
+                LOGGER.error("Unexpected exception when parsing section {} of {}", sectionState.getSectionNumber(), sectionState.getDocumentState().getDocumentId(), e);
+            }
+            return new ParseResult(elements.toImmutable(), e);
         }
     }
 
-    protected void parse(SectionSourceCode section, Consumer<PackageableElement> elementConsumer)
+    protected void parse(SectionState section, Consumer<PackageableElement> elementConsumer)
     {
         parse(section, elementConsumer, new PureGrammarParserContext(PureGrammarParserExtensions.fromAvailableExtensions()));
+    }
+
+    protected void parse(SectionState section, Consumer<PackageableElement> elementConsumer, PureGrammarParserContext pureGrammarParserContext)
+    {
+        parse(toSectionSourceCode(section), elementConsumer, pureGrammarParserContext);
     }
 
     protected abstract void parse(SectionSourceCode section, Consumer<PackageableElement> elementConsumer, PureGrammarParserContext pureGrammarParserContext);
@@ -166,5 +188,52 @@ abstract class AbstractLSPGrammarExtension implements LegendLSPGrammarExtension
     protected static TextInterval toLocation(SourceInformation sourceInfo)
     {
         return TextInterval.newInterval(sourceInfo.startLine, sourceInfo.startColumn - 1, sourceInfo.endLine, sourceInfo.endColumn - 1);
+    }
+
+    protected static class ParseResult
+    {
+        private final ImmutableList<PackageableElement> elements;
+        private final Exception e;
+
+        private ParseResult(ImmutableList<PackageableElement> elements, Exception e)
+        {
+            this.elements = elements;
+            this.e = e;
+        }
+
+        private ParseResult(ImmutableList<PackageableElement> elements)
+        {
+            this(elements, null);
+        }
+
+        public ImmutableList<PackageableElement> getElements()
+        {
+            return this.elements;
+        }
+
+        public void forEachElement(Consumer<? super PackageableElement> consumer)
+        {
+            this.elements.forEach(consumer);
+        }
+
+        public boolean hasException()
+        {
+            return this.e != null;
+        }
+
+        public Exception getException()
+        {
+            return this.e;
+        }
+
+        public boolean hasEngineException()
+        {
+            return this.e instanceof EngineException;
+        }
+
+        public EngineException getEngineException()
+        {
+            return hasEngineException() ? (EngineException) this.e : null;
+        }
     }
 }
