30d06e18f3c3316f29e681eb3cafc88ee3405b77
kevin-m-knight-gs <kevin.knight@gs.com>
2023-11-14 21:10:18 -0500

    Add support for running legacy service and mapping tests

--- legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/ServiceLSPGrammarExtension.java
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/ServiceLSPGrammarExtension.java
@@ -14,14 +14,33 @@
 
 package org.finos.legend.engine.ide.lsp.extension;
 
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.list.MutableList;
 import org.eclipse.collections.impl.utility.Iterate;
+import org.finos.legend.engine.ide.lsp.extension.execution.LegendExecutionResult;
+import org.finos.legend.engine.ide.lsp.extension.execution.LegendExecutionResult.Type;
+import org.finos.legend.engine.ide.lsp.extension.state.SectionState;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
 import org.finos.legend.engine.language.pure.dsl.service.grammar.from.ServiceParserExtension;
+import org.finos.legend.engine.plan.execution.PlanExecutor;
+import org.finos.legend.engine.plan.generation.extension.PlanGeneratorExtension;
+import org.finos.legend.engine.plan.generation.transformers.PlanTransformer;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.PackageableElement;
 import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.service.Service;
+import org.finos.legend.engine.protocol.pure.v1.model.packageableElement.service.ServiceTestSuite;
 import org.finos.legend.engine.protocol.pure.v1.model.test.TestSuite;
+import org.finos.legend.engine.pure.code.core.PureCoreExtensionLoader;
+import org.finos.legend.engine.test.runner.service.RichServiceTestResult;
+import org.finos.legend.engine.test.runner.service.ServiceTestRunner;
+import org.finos.legend.engine.test.runner.shared.TestResult;
+import org.finos.legend.pure.generated.Root_meta_pure_extension_Extension;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Collections;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Map;
+import java.util.ServiceLoader;
 
 /**
  * Extension for the Service grammar.
@@ -29,6 +48,10 @@ import java.util.function.Consumer;
 public class ServiceLSPGrammarExtension extends AbstractSectionParserLSPGrammarExtension
 {
     private static final List<String> KEYWORDS = List.of("Service", "import");
+
+    private static final String RUN_LEGACY_TESTS_COMMAND_ID = "legend.service.runLegacyTests";
+    private static final String RUN_LEGACY_TESTS_COMMAND_TITLE = "Run legacy tests";
+
 
     public ServiceLSPGrammarExtension()
     {
@@ -48,21 +71,117 @@ public class ServiceLSPGrammarExtension extends AbstractSectionParserLSPGrammarE
     }
 
     @Override
-    protected void forEachTestSuite(PackageableElement element, Consumer<? super TestSuite> consumer)
+    protected List<? extends TestSuite> getTestSuites(PackageableElement element)
     {
         if (element instanceof Service)
         {
-            ((Service) element).testSuites.forEach(consumer);
+            List<ServiceTestSuite> testSuites = ((Service) element).testSuites;
+            return (testSuites == null) ? Lists.fixedSize.empty() : testSuites;
+        }
+        return super.getTestSuites(element);
+    }
+
+    @Override
+    protected void collectCommands(SectionState sectionState, PackageableElement element, CommandConsumer consumer)
+    {
+        super.collectCommands(sectionState, element, consumer);
+        if (element instanceof Service)
+        {
+            Service service = (Service) element;
+            if (service.test != null)
+            {
+                consumer.accept(RUN_LEGACY_TESTS_COMMAND_ID, RUN_LEGACY_TESTS_COMMAND_TITLE, service.sourceInformation);
+            }
         }
     }
 
     @Override
-    protected TestSuite findTestSuite(PackageableElement element, String id)
+    public Iterable<? extends LegendExecutionResult> execute(SectionState section, String entityPath, String commandId, Map<String, String> executableArgs)
     {
-        if (element instanceof Service)
+        return RUN_LEGACY_TESTS_COMMAND_ID.equals(commandId) ?
+                runLegacyServiceTest(section, entityPath) :
+                super.execute(section, entityPath, commandId, executableArgs);
+    }
+
+    private Iterable<? extends LegendExecutionResult> runLegacyServiceTest(SectionState section, String entityPath)
+    {
+        PackageableElement element = getParseResult(section).getElement(entityPath);
+        if (!(element instanceof Service))
         {
-            return Iterate.detect(((Service) element).testSuites, ts -> id.equals(ts.id));
+            return Collections.singletonList(LegendExecutionResult.newResult(Type.ERROR, "Unable to find service " + entityPath));
         }
-        return null;
+        Service service = (Service) element;
+        if (service.test == null)
+        {
+            return Collections.singletonList(LegendExecutionResult.newResult(Type.ERROR, "Unable to find legacy test for service " + entityPath));
+        }
+
+        CompileResult compileResult = getCompileResult(section);
+        if (compileResult.hasException())
+        {
+            return Collections.singletonList(errorResult(compileResult.getException()));
+        }
+
+        PureModel pureModel = compileResult.getPureModel();
+        MutableList<? extends Root_meta_pure_extension_Extension> routerExtensions = PureCoreExtensionLoader.extensions().flatCollect(e -> e.extraPureCoreExtensions(pureModel.getExecutionSupport()));
+        MutableList<PlanTransformer> planTransformers = Iterate.flatCollect(ServiceLoader.load(PlanGeneratorExtension.class), PlanGeneratorExtension::getExtraPlanTransformers, Lists.mutable.empty());
+        ServiceTestRunner testRunner = new ServiceTestRunner(service, null, compileResult.getPureModelContextData(), pureModel, null, PlanExecutor.newPlanExecutorBuilder().withAvailableStoreExecutors().build(), routerExtensions, planTransformers, null);
+
+        List<RichServiceTestResult> richServiceTestResults;
+        try
+        {
+            richServiceTestResults = testRunner.executeTests();
+        }
+        catch (Exception e)
+        {
+            return Collections.singletonList(errorResult(compileResult.getException()));
+        }
+
+        MutableList<LegendExecutionResult> results = Lists.mutable.empty();
+        richServiceTestResults.forEach(run ->
+        {
+            Map<String, TestResult> runResults = run.getResults();
+            Map<String, Exception> runExceptions = run.getAssertExceptions();
+            if (runResults != null)
+            {
+                runResults.forEach((key, result) ->
+                {
+                    StringWriter writer = new StringWriter().append(entityPath).append('.').append(key).append(": ").append(result.name());
+                    Exception e = runExceptions.get(key);
+                    if (e != null)
+                    {
+                        try (PrintWriter pw = new PrintWriter(writer.append("\n")))
+                        {
+                            e.printStackTrace(pw);
+                        }
+                    }
+                    results.add(LegendExecutionResult.newResult(toResultType(result), writer.toString()));
+                });
+            }
+        });
+        return results;
+    }
+
+    private Type toResultType(TestResult testResult)
+    {
+        switch (testResult)
+        {
+            case SUCCESS:
+            {
+                return Type.SUCCESS;
+            }
+            case FAILURE:
+            {
+                return Type.FAILURE;
+            }
+            case ERROR:
+            {
+                return Type.ERROR;
+            }
+            default:
+            {
+                return Type.WARNING;
+            }
+        }
     }
 }
