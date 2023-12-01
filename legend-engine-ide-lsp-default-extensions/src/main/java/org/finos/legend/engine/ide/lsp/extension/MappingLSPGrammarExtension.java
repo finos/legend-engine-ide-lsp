28c8a607b66d882ad7e1e0d323f6d46dd2d2cfa0
kevin-m-knight-gs <kevin.knight@gs.com>
2023-12-01 09:04:56 -0500

    Update mapping grammar extension keywords

--- legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/MappingLSPGrammarExtension.java
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/MappingLSPGrammarExtension.java
@@ -15,13 +15,17 @@
 package org.finos.legend.engine.ide.lsp.extension;
 
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.factory.Sets;
+import org.eclipse.collections.api.list.ListIterable;
 import org.eclipse.collections.api.list.MutableList;
+import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.impl.utility.Iterate;
 import org.eclipse.collections.impl.utility.ListIterate;
 import org.finos.legend.engine.ide.lsp.extension.execution.LegendExecutionResult;
 import org.finos.legend.engine.ide.lsp.extension.execution.LegendExecutionResult.Type;
 import org.finos.legend.engine.ide.lsp.extension.state.SectionState;
 import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.language.pure.grammar.from.extension.PureGrammarParserExtensionLoader;
 import org.finos.legend.engine.language.pure.grammar.from.extension.PureGrammarParserExtensions;
 import org.finos.legend.engine.language.pure.grammar.from.mapping.MappingParser;
 import org.finos.legend.engine.plan.execution.PlanExecutor;
@@ -49,8 +53,6 @@ import java.util.ServiceLoader;
  */
 public class MappingLSPGrammarExtension extends AbstractLegacyParserLSPGrammarExtension
 {
-    private static final List<String> KEYWORDS = List.of("Mapping", "EnumerationMapping", "include");
-
     private static final String RUN_LEGACY_TESTS_COMMAND_ID = "legend.mapping.runLegacyTests";
     private static final String RUN_LEGACY_TESTS_COMMAND_TITLE = "Run legacy tests";
 
@@ -58,15 +60,18 @@ public class MappingLSPGrammarExtension extends AbstractLegacyParserLSPGrammarEx
     private static final String RUN_LEGACY_TEST_COMMAND_TITLE = "Run legacy test";
     private static final String LEGACY_TEST_ID = "legend.mapping.legacyTestId";
 
+    private final ListIterable<String> keywords;
+
     public MappingLSPGrammarExtension()
     {
         super(MappingParser.newInstance(PureGrammarParserExtensions.fromAvailableExtensions()));
+        this.keywords = findKeywords();
     }
 
     @Override
     public Iterable<? extends String> getKeywords()
     {
-        return KEYWORDS;
+        return this.keywords;
     }
 
     @Override
@@ -207,5 +212,12 @@ public class MappingLSPGrammarExtension extends AbstractLegacyParserLSPGrammarEx
             return tests;
         }
         return ListIterate.select(tests, t -> testName.equals(t.name));
+    }
+
+    private static ListIterable<String> findKeywords()
+    {
+        MutableSet<String> keywords = Sets.mutable.with("Mapping", "MappingTests", "include");
+        PureGrammarParserExtensionLoader.extensions().forEach(ext -> ext.getExtraMappingElementParsers().forEach(p -> keywords.add(p.getElementTypeName())));
+        return Lists.immutable.withAll(keywords);
     }
 }
