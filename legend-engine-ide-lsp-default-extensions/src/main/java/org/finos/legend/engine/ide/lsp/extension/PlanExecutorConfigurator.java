e6459e53ffa449e7ada55f89a118bdecaaaf7e6c
Rafael Bey <24432403+rafaelbey@users.noreply.github.com>
2024-08-30 16:22:24 -0400

    Relational Executor setup changes (#233)

--- legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/PlanExecutorConfigurator.java
+++ legend-engine-ide-lsp-default-extensions/src/main/java/org/finos/legend/engine/ide/lsp/extension/PlanExecutorConfigurator.java
@@ -16,43 +16,73 @@
 
 package org.finos.legend.engine.ide.lsp.extension;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.eclipse.collections.api.factory.Maps;
-import org.finos.legend.engine.plan.execution.PlanExecutor;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import org.eclipse.collections.api.factory.Maps;
+import org.finos.legend.engine.plan.execution.PlanExecutor;
+import org.finos.legend.engine.plan.execution.stores.StoreExecutorBuilderLoader;
+import org.finos.legend.engine.plan.execution.stores.StoreExecutorConfiguration;
+import org.finos.legend.engine.plan.execution.stores.StoreType;
+import org.finos.legend.engine.shared.core.operational.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface PlanExecutorConfigurator extends LegendLSPFeature
 {
-    PlanExecutor.Builder configure(PlanExecutor.Builder builder, Map<String, Object> config);
+    Logger LOGGER = LoggerFactory.getLogger(PlanExecutorConfigurator.class);
+
+    StoreExecutorConfiguration configure(Map<String, Object> config);
 
     static PlanExecutor create(Path planExecutorConfigurationJsonPath, List<LegendLSPFeature> features)
     {
-        Map<String, Object> planExecutorConfiguration;
-        try
+        Map<String, Object> planExecutorConfiguration = Maps.mutable.empty();
+        if (planExecutorConfigurationJsonPath != null)
         {
-            planExecutorConfiguration = planExecutorConfigurationJsonPath == null
-                    ? Maps.mutable.empty()
-                    : new ObjectMapper().readValue(new String(Files.readAllBytes(planExecutorConfigurationJsonPath)), new TypeReference<>(){});
+            try (InputStream is = Files.newInputStream(planExecutorConfigurationJsonPath))
+            {
+                planExecutorConfiguration = new ObjectMapper().readValue(is, Map.class);
+            }
+            catch (IOException e)
+            {
+                LOGGER.error("Unable to parse JSON content on config file: {}", planExecutorConfigurationJsonPath, e);
+            }
         }
-        catch (IOException e)
-        {
-            throw new RuntimeException(e);
-        }
-        PlanExecutor.Builder builder = PlanExecutor.newPlanExecutorBuilder();
+
+        Map<StoreType, StoreExecutorConfiguration> storeTypeStoreExecutorConfigurationMap = Maps.mutable.empty();
+
         for (LegendLSPFeature feature : features)
         {
             if (feature instanceof PlanExecutorConfigurator)
             {
-                builder = ((PlanExecutorConfigurator) feature).configure(builder, planExecutorConfiguration);
+                StoreExecutorConfiguration storeExecutorConfiguration = ((PlanExecutorConfigurator) feature).configure(planExecutorConfiguration);
+                StoreType storeType = storeExecutorConfiguration.getStoreType();
+                StoreExecutorConfiguration existing = storeTypeStoreExecutorConfigurationMap.put(storeType, storeExecutorConfiguration);
+                Assert.assertTrue(existing == null, () -> "Found multiple configurations for store type: " + storeType);
             }
         }
 
-        return builder.withAvailableStoreExecutors().build();
+        PlanExecutor.Builder builder = PlanExecutor.newPlanExecutorBuilder();
+
+        StoreExecutorBuilderLoader.extensions().stream().map(s ->
+        {
+            StoreExecutorConfiguration storeExecutorConfiguration = storeTypeStoreExecutorConfigurationMap.remove(s.getStoreType());
+            if (storeExecutorConfiguration == null)
+            {
+                return s.build();
+            }
+            else
+            {
+                return s.build(storeExecutorConfiguration);
+            }
+        }).forEach(builder::withStoreExecutors);
+
+        Assert.assertTrue(storeTypeStoreExecutorConfigurationMap.isEmpty(), () -> "Collected store configuration with no store executor builder: " + storeTypeStoreExecutorConfigurationMap.keySet());
+
+        return builder.build();
     }
 }
